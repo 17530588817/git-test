2a5cc0ffa8d9512ebe781f7af6f6ee799744a8e6
17530588817 <93063121+17530588817@users.noreply.github.com>
2021-11-03 13:50:05 +0800

    pull test

--- src/main/java/com/git/GitTest1.java
+++ src/main/java/com/git/GitTest1.java
@@ -9,9 +9,8 @@ public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("这是第一个Git 11111");
-        System.out.println("这是第一个Git 22222");
-        System.out.println("这是第一个Git 333333");
+    
         System.out.println("hot-fix");
-        System.out.println("这是第一个Git");
+        System.out.println("pull test");
     }
 }
