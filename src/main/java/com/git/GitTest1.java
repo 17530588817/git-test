ce6e4d73a4be853cdf12f02869fadb34dcdf30af
hengbao <2580986211@qq.com>
2021-11-03 13:08:13 +0800

    master commit2

--- src/main/java/com/git/GitTest1.java
+++ src/main/java/com/git/GitTest1.java
@@ -7,7 +7,7 @@ package com.git;
  */
 public class GitTest1 {
     public static void main(String[] args) {
-        System.out.println("hot-fix");
+        System.out.println("master");
         System.out.println("这是第一个Git 11111");
         System.out.println("这是第一个Git 22222");
         System.out.println("这是第一个Git 333333");
