b456c8a004c02310341cf1718f1124ae28ebb9f8
hengbao <2580986211@qq.com>
2021-11-03 13:07:35 +0800

    hot-fix 2 commit

--- src/main/java/com/git/GitTest1.java
+++ src/main/java/com/git/GitTest1.java
@@ -8,8 +8,6 @@ package com.git;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hot-fix");
-        System.out.println("这是第一个Git 11111");
-        System.out.println("这是第一个Git 22222");
-        System.out.println("这是第一个Git 333333");
+        System.out.println("这是第一个Git");
     }
 }
